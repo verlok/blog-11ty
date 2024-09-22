df9127c3245e286a6049dc9e8a78a8bc54248e45
Andrea Verlicchi <verlok@users.noreply.github.com>
2024-09-22 17:28:17 +0200

    Added images in posts list!

--- eleventy.config.images.js
+++ eleventy.config.images.js
@@ -1,7 +1,8 @@
 const path = require("path");
 const eleventyImage = require("@11ty/eleventy-img");
+const { JSDOM } = require("jsdom");
 
-module.exports = eleventyConfig => {
+module.exports = (eleventyConfig) => {
 	function relativeToInputPath(inputPath, relativeFilePath) {
 		let split = inputPath.split("/");
 		split.pop();
@@ -11,31 +12,75 @@ module.exports = eleventyConfig => {
 
 	// Eleventy Image shortcode
 	// https://www.11ty.dev/docs/plugins/image/
-	eleventyConfig.addAsyncShortcode("image", async function imageShortcode(src, alt, widths, sizes, useLazyLoading, className) {
-		// Full list of formats here: https://www.11ty.dev/docs/plugins/image/#output-formats
-		// Commented AVIF in July 6th because Safari tries to render it, but it doesn't
-		let formats = [/* "avif",  */"webp", "auto"];
-		let file = relativeToInputPath(this.page.inputPath, src);
-		let metadata = await eleventyImage(file, {
-			widths: widths || ["auto"],
-			formats,
-			outputDir: path.join(eleventyConfig.dir.output, "img"), // Advanced usage note: `eleventyConfig.dir` works here because we’re using addPlugin.
-		});
-
-		// Find a way to pass loading lazy to second to more images
-		const loading = useLazyLoading ? "lazy" : "eager";
-		let imageAttributes = {
+	eleventyConfig.addAsyncShortcode(
+		"image",
+		async function imageShortcode(
+			src,
 			alt,
+			widths,
 			sizes,
-			loading,
-			decoding: "async",
-		};
-		if (className) {
-			imageAttributes.class = className;
+			useLazyLoading,
+			className
+		) {
+			// Full list of formats here: https://www.11ty.dev/docs/plugins/image/#output-formats
+			// Commented AVIF in July 6th because Safari tries to render it, but it doesn't
+			let formats = [/* "avif",  */ "webp", "auto"];
+			let file = relativeToInputPath(this.page.inputPath, src);
+			let metadata = await eleventyImage(file, {
+				widths: widths || ["auto"],
+				formats,
+				outputDir: path.join(eleventyConfig.dir.output, "img"), // Advanced usage note: `eleventyConfig.dir` works here because we’re using addPlugin.
+			});
+
+			// Find a way to pass loading lazy to second to more images
+			const loading = useLazyLoading ? "lazy" : "eager";
+			let imageAttributes = {
+				alt,
+				sizes,
+				loading,
+				decoding: "async",
+			};
+			if (className) {
+				imageAttributes.class = className;
+			}
+			if (!useLazyLoading) {
+				imageAttributes.fetchpriority = "high";
+			}
+			return eleventyImage.generateHTML(metadata, imageAttributes);
 		}
-		if (!useLazyLoading) {
-			imageAttributes.fetchpriority = "high";
+	);
+	/*
+	eleventyConfig.addShortcode("first-image", (post) => {
+		extractFirstImage(post);
+	});
+
+	const extractFirstImage = (templateContent) => {
+		if (templateContent.includes("<picture>")) {
+			const pictureTagBegin = templateContent.indexOf("<picture>");
+			const pictureTagEnd =
+				templateContent.indexOf("</picture>", pictureTagBegin) +
+				"</picture>".length;
+			//console.log(pictureTagBegin, pictureTagEnd);
+			//console.log(templateContent.substring(pictureTagBegin, pictureTagEnd));
+			return "Cippa lippa";
+			return templateContent.substring(pictureTagBegin, pictureTagEnd);
 		}
-		return eleventyImage.generateHTML(metadata, imageAttributes);
+
+		return "";
+	};
+	*/
+
+	eleventyConfig.addFilter("extractImage", function (content) {
+		const dom = new JSDOM(content);
+		const fig = dom.window.document.querySelector("figure");
+		if (!fig) return "";
+		const picture = fig.querySelector("picture");
+		const img = fig.querySelector("img");
+		const returnedTag = picture || img;
+		if (returnedTag) {
+			returnedTag.classList.add('postlist-image')
+			return returnedTag.outerHTML;
+		}
+		return "";
 	});
 };
